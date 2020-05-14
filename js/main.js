607f7b6095554a8c9d6eedb97ba331997046af48
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-05-14 14:43:02 -0400

    Add JS File

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// console.log("JS file is linked")
+
+// let firstName = "Marco";
+// let age = 40;
+
+// console.log(`hello ${firstName}`); /* new way, ES6 template Literals */
+
+// console.log("Hello" + firstName);
+
+// crtl + /   for commet out everthing
